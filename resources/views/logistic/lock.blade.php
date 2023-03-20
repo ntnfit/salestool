d65828a357da16ea9fda21f9df30bac182862458
Hinh Thanh Thien Trang <Trang.Hinh@vn.gt.com>
2023-03-20 13:06:23 +0700

    updatelockview

--- resources/views/logistic/lock.blade.php
+++ resources/views/logistic/lock.blade.php
@@ -1,5 +1,6 @@
 @extends('adminlte::page')
 
+<<<<<<< HEAD
 @section('title', 'lock vehicle')
 @section('plugins.Datatables', true)
 
@@ -9,3 +10,106 @@
 
 @section('content')
 @stop
+=======
+@section('title', 'Lock Vehicle')
+@section('plugins.BsCustomFileInput', true)
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <script src="https://unpkg.com/jquery/dist/jquery.min.js"></script>	
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js"></script>
+
+@section('content')
+@php
+
+@endphp
+
+<h4> Lock/ Unlock Vehicle </h4>
+<div class="content">
+    <div id="MyGrid" class="ag-theme-alpine" style="height: 80%">
+      </div>
+     
+    <x-adminlte-button class="btn-flat" id="lockall" style="float: left;font-size: small; margin-top:30px;" type="button" label="Lock All" theme="success"/>
+    <x-adminlte-button class="btn-flat" id="unall" style="float: left;font-size: small;margin-top:30px; margin-left: 20px;" type="button" label="Unlock All" theme="success"/>
+    <x-adminlte-button class="btn-flat" id="export" style="float: right;font-size: small;margin-top:30px;" type="button" label="Export Excel" theme="success"/>
+</div>
+
+
+@endsection
+
+
+@section('css')
+@stop
+
+
+@push('js')
+<script>
+
+var __basePath = './';</script>
+		<script src="https://cdn.jsdelivr.net/npm/ag-grid-community@28.2.1/dist/ag-grid-community.min.js"> 
+		</script>
+    <script src="https://cdn.jsdelivr.net/npm/ag-grid-enterprise@28.2.1/dist/ag-grid-enterprise.min.js">
+      </script>
+      <script>
+      var filterParams = {
+  comparator: (filterLocalDateAtMidnight, cellValue) => {
+    var dateAsString = cellValue;
+    if (dateAsString == null) return -1;
+    var dateParts = dateAsString.split('/');
+    var cellDate = new Date(
+      Number(dateParts[2]),
+      Number(dateParts[1]) - 1,
+      Number(dateParts[0])
+    );
+
+    if (filterLocalDateAtMidnight.getTime() === cellDate.getTime()) {
+      return 0;
+    }
+
+    if (cellDate < filterLocalDateAtMidnight) {
+      return -1;
+    }
+
+    if (cellDate > filterLocalDateAtMidnight) {
+      return 1;
+    }
+  },
+  browserDatePicker: true,
+};
+
+const columnDefs = [
+    { field: 'Truck Code'},
+    { field: 'Truck Driver'},
+    { field: 'Truck Type'},
+    { field: 'Capacity'},
+    { field: 'Phone'},
+    { field: 'Status'},
+    { field: 'Lock',
+        },
+    { field: 'Unlock'},
+
+];
+
+
+
+const gridOptions = {
+  columnDefs: columnDefs,
+  pagination: true,
+  defaultColDef: {
+    flex: 1,
+    minWidth: 150,
+    filter: true,
+    resizable: true,
+  },
+};
+function onBtExport() {
+  gridOptions.api.exportDataAsExcel();
+}
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function () {
+  var gridDiv = document.querySelector('#myGrid');
+  new agGrid.Grid(gridDiv, gridOptions);
+  gridOptions.api.setRowData("")
+    });
+
+</script>
+@endpush
+>>>>>>> 2cc3811 (update)
