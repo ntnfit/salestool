a4edb1af9053d016147f12824000f43a7be1a312
harrydevnguyen <ntnfit98@gmail.com>
2023-06-07 14:59:37 +0700

    update fix

--- app/Http/Controllers/sap/SalesController.php
+++ app/Http/Controllers/sap/SalesController.php
@@ -117,28 +117,18 @@ class SalesController extends Controller
     function loadall()
     {
         $conDB = (new SAPB1Controller)->connect_sap();
-     
-        $sql = 'CALL USP_BS_STOCKOUTREQUEST2(?,?,?,?)';
+        $sql = 'select TOP 10000 * from "UV_SO_LOADALL"';
         $stmt = odbc_prepare($conDB, $sql);
-        // Set the input parameters for the stored procedure
-        $promotionType = '';
-        
-        $special = 0;
-
-        // Execute the stored procedure with the input parameters
-        if (!odbc_execute($stmt, array($promotionType,"", "", $special))) {
-            // Handle execution error
-            die("Error executing SQL statement: " . odbc_errormsg());
-        }
-
-        // Retrieve the result set from the stored procedure
+        odbc_execute($stmt);
         $results = array();
         while ($row = odbc_fetch_array($stmt)) {
-            $results[] = $row;
+        $results[] = $row;
         }
-        $results=json_encode($results);
         odbc_close($conDB);
-        return  $results;
+        $results = json_encode($results);
+
+        return $results;
+
     }
     function edit($id)
     {
